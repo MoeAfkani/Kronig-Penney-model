7cf83a000069d83cbde4d696598f997a36b530ce
Moe <mafkani@mun.ca>
2022-01-22 10:55:55 -0330

    inti succ

--- KronigPenneymodel.c
+++ KronigPenneymodel.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
+/*
 double a = 5e-10;               // Lattice Const.
 double b = 1e-10;               // Step Len.
 double v0 = 1;                  // Potential depth eV
@@ -9,30 +11,40 @@ double dE = 0.001;              // Energy step eV
 double h_bar = 6.582119569e-16; //eV.s
 double m_e = 9.10938356e-31;    //e mass Kg;
 double e_const = 1.602e-19;
+*/
+double a = 10;
+double b = 8;
+double v0 = 3;
+double E;
+double dE = 0.000001;
+double h_bar = 1;
+double m_e = 1;
+double e_const = 1;
 
-double K_E(double, double, double, double);
-aljshalshdljhsdl
+double cos_K_E(double, double, double, double);
+
 int main(void)
 {
-    for (E = 0; abs(E) < v0; E -= dE)
+    FILE *bandstrc;
+    bandstrc = fopen("bandstrc.dat","w");
+    for (E = -0.5; abs(E) < v0; E -= dE)
     {
-        printf("%f", K_E(a, b, v0, E));
+        if (fabs(cos_K_E(a, b, v0, E)) <= 1)
+        {
+            fprintf(bandstrc, "%f\t%f\n" , E, acos(cos_K_E(a, b, v0, E))/M_PI);
+        }
     }
+    fclose(bandstrc);
     return 0;
 }
 
-double K_E(double a, double b, double v0, double E)
+double cos_K_E(double a, double b, double v0, double E)
 {
 
-    double alpha = sqrt(2 * m_e * abs(E)) / h_bar;
-    double beta = sqrt(2 * m_e * (v0 - abs(E))) / h_bar;
+    double alpha = sqrt(2 * m_e * fabs(E)) / h_bar;
+    double beta = sqrt(2 * m_e * (v0 - fabs(E))) / h_bar;
     double d = b - a;
     double cos_ka = cos(beta * b) * cosh(alpha * d) - (beta * beta - alpha * alpha) / (2 * alpha * beta) * sin(beta * b) * sinh(alpha * d);
-    //printf("%f",cos_ka);
-    if (cos_ka <= 0)
-    {
-        double k = acos(cos_ka) / a;
-        return k;
-    }
-    return 0;
+    //printf("%f\n",cos_ka);
+    return cos_ka;
 }
