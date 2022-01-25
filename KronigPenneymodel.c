2acfb9c0c74d0ca3893fdfbb4a0ed22f4223b1ac
Moe <mafkani@mun.ca>
2022-01-25 10:04:58 -0330

    Min Max found

--- KronigPenneymodel.c
+++ KronigPenneymodel.c
@@ -7,33 +7,58 @@
 double a = 5e-10;               // Lattice Const.
 double b = 1e-10;               // Step Len.
 double v0 = 1;                  // Potential depth eV
-double E;                       // Energy eV
-double dE = 0.001;              // Energy step eV
+double dE = 0.0001;              // Energy step eV
 double h_bar = 6.582119569e-16; //eV.s
 double m_e = 9.10938356e-31;    //e mass Kg;
 double e_const = 1.602e-19;
-*/ 
+*/
 double a = 10;
 double b = 8;
 double v0 = 1.5;
-double E;
 double dE = 0.0001;
 double h_bar = 1;
 double m_e = 1;
 double e_const = 1;
+int bandNumber = 4;
+int bandNumber2 = 4;
 
 double cos_K_E(double, double, double, double);
 
 int main(int argc, char *argv[])
 {
+    int band_i = 0;
+    double ETest = 0;
+    double maxEs[bandNumber];
+    double minEs[bandNumber2];
     FILE *bandstrc;
-    bandstrc = fopen("bandstrc.dat","w");
-    int bandNumber = 1;
-    for (E = 0; abs(E) <= v0; E += dE)
+    //FILE *outscf;
+    bandstrc = fopen("bandstrc.dat", "w");
+    //outscf = fopen("scf.dat","w");
+    for (double E = 0; abs(E) <= v0; E += dE)
     {
+
         if (fabs(cos_K_E(a, b, v0, E)) <= 1)
         {
-            fprintf(bandstrc, "%f\t%f\t%d\n" , E, acos(cos_K_E(a, b, v0, E))/M_PI,bandNumber);
+            if (fabs(E - ETest) > 1 * dE)
+            {
+                band_i += 1;
+                minEs[band_i] = E;
+                maxEs[band_i] = E;
+            }
+            ETest = E;
+
+            if (maxEs[band_i] < E)
+            {
+                maxEs[band_i] = E;
+            }
+
+            if (minEs[band_i] > E)
+            {
+                minEs[band_i] = E;
+            }
+
+            fprintf(bandstrc, "%f\t%f\t%d\tMax:%f\tMin:%f\n", E, acos(cos_K_E(a, b, v0, E)) / M_PI, band_i,maxEs[band_i],minEs[band_i]);
+    
         }
     }
     fclose(bandstrc);
@@ -42,11 +67,10 @@ int main(int argc, char *argv[])
 
 double cos_K_E(double a, double b, double v0, double E)
 {
-
     double alpha = sqrt(2 * m_e * fabs(E)) / h_bar;
     double beta = sqrt(2 * m_e * (v0 - fabs(E))) / h_bar;
     double d = b - a;
     double cos_ka = cos(beta * b) * cosh(alpha * d) - (beta * beta - alpha * alpha) / (2 * alpha * beta) * sin(beta * b) * sinh(alpha * d);
-    //printf("%f\n",cos_ka);
+    // printf("%f\n",cos_ka);
     return cos_ka;
 }
--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ gcc KronigPenneymodel.c -o KronigPenneymodel -lm;
 echo "Compiled";
 ./KronigPenneymodel
 echo "run";
-python3 plot.py;
+# python3 plot.py;
 echo "plot":
